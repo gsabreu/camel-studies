2fd337b315b7754ec004548e6a3e563c4aa8a362
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-08-22 19:38:29 -0300

    Added Spring boot example

---
+++ camel-spring-boot-projeto/src/main/java/br/com/caelum/camel/route/ProdutoService.java
@@ -0,0 +1,15 @@
+package br.com.caelum.camel.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+	from("file:pedidos").
+	to("activemq:queue:pedidos");
+    }
+
+}
