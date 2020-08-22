541e29ff5a8fc29f4f4832163b03579630816caa
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-08-22 16:58:26 -0300

    Send XML with SOAP

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -33,15 +33,15 @@ public class RotaPedidos {
 				xpath("/item/formato[text()='EBOOK']").
 			setProperty("ebookId", xpath("/item/livro/codigo/text()")).
 			marshal().xmljson().
-			log("${id} - ${body}").
 			setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
 			setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
 		to("http4://localhost:8080/webservices/ebook/item");
 		
 		from("direct:soap").
 			routeId("rota-soap").
-			setBody(constant("<envelope>teste</envelope>")).
-		to("mock:soap");
+			to("xslt:pedido-para-soap.xslt").
+			setHeader(Exchange.CONTENT_TYPE, constant("text/xml")).
+		to("http4://localhost:8080/webservices/financeiro");
 
 	    }
 	});
